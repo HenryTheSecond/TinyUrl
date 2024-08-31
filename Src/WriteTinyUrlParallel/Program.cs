32a8a73ef9db9599be8645c715e6cc6f716674a0
HenryTheSecond <92526096+HenryTheSecond@users.noreply.github.com>
2024-08-31 23:59:40 +0700

    add project for concurrent testing

---
+++ Src/WriteTinyUrlParallel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+HttpClient httpClient = new()
+{
+    BaseAddress = new Uri("https://localhost:7201")
+};
+
+var tasks = Enumerable.Range(0, 10000).Select(x => Task.Run(async () =>
+{
+    var response = await httpClient.PostAsync("createTinyUrl", new StringContent("{\r\n  \"originalUrl\": \"string\"\r\n}", Encoding.UTF8, "application/json"));
+    response.EnsureSuccessStatusCode();
+    var a = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(a);
+})).ToArray();
+
+Task.WaitAll(tasks);
+
+Console.WriteLine();
